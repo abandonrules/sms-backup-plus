e7d7cc68bc974564ddec12ef8e98393f4f681b84
Kleis Auke Wolthuizen <github@kleisauke.nl>
2018-05-24 23:11:04 +0200

    Use Gradle dependency instead of static files

--- app/src/main/java/com/github/jberkel/whassup/DBProvider.java
+++
@@ -1,8 +0,0 @@
-package com.github.jberkel.whassup;
-
-import java.io.File;
-
-public interface DBProvider {
-    /** @return the current DB, which is guaranteed to be present and readable */
-    File getDBFile();
-}
